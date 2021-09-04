9b390125125dceab48954cfac05b3cb49cedaf85
Akardian <mike.wuestenberg@gmail.com>
2021-09-04 13:50:48 +0200

    add hardlink script

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Home for small scripts
 #### open-multiple-terminals.sh
     
 Can be executed to open a terminal with diffrent tabs and tasks
+
+#### HardLinkConfig
+
+Creates a hardlink to a savegame location to save storage on c:
---
+++ windows/HardLinkConfig.bat
@@ -0,0 +1,19 @@
+﻿echo off 
+chcp 65001
+SET SCRIPT_LOCATION=%~dp0
+SET SAVEGA_LOCATION=%appdata%/
+SET GAME_NAME=SpaceEngineers
+
+echo.
+echo Create Link
+echo Game: %GAME_NAME%
+echo.
+echo SCRIPT_LOCATION: %SCRIPT_LOCATION%
+echo SAVEGA_LOCATION: %SAVEGA_LOCATION%%GAME_NAME%
+echo.
+
+if not exist "%SAVEGA_LOCATION%" mkdir "%SAVEGA_LOCATION%
+mklink /D "%SAVEGA_LOCATION%%GAME_NAME%" "%SCRIPT_LOCATION%"
+
+echo. 
+pause
